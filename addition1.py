e120c72b643e74aa9f8ee9ceb218ad1f1444808c
‘prasanna0324’ <prasannasree03@gmail.com>
2025-08-21 15:35:14 +0530

    python

---
+++ addition1.py
@@ -0,0 +1,3 @@
+a=input("enter first number=100")
+b=input("enter second number=200")
+print(a+b)
---
+++ addition4.py
@@ -0,0 +1,6 @@
+num=int(input("enter number:"))
+if num >=100 and num <=999:
+    pass
+else:
+    print("no")
+    
